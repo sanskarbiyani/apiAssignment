c96216539e8b051e51434e2499166e741f4fdc19
Sanskar Biyani <sanskarbiyani902@gmail.com>
2022-01-15 01:23:44 +0530

    Implementation of API2

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const {MongoClient} = require("mongodb");
+// const request = require("request")
+// const https = require("https")
+const axios = require('axios')
 var ObjectId = require("mongodb").ObjectId;
+const { add } = require("nodemon/lib/rules");
 
 const app = express();
 const port = 3000;
@@ -12,12 +16,21 @@ async function getData(url, collection1, collection2){
     const client = new MongoClient(url);
     try{
         await client.connect();
-        const devices = client.db('__CONCOX__').collection(collection1)
+        const database = client.db('__CONCOX__')
+        const devices = database.collection(collection1)
+        const status = database.collection(collection2);
         const device = devices.find({}).sort("createdAt", -1).limit(30)
         var arr = [];
         await device.forEach(doc => {
-            arr.push(doc.id)
+            arr.push(doc.imei)
         });
+        for(var i=0; i<arr.length; ++i){
+            console.log(`\nFor ${arr[i]}`)
+            const locations = status.find({gps: {$ne: null}, imei: `${arr[i]}`}).count()
+            // console.log(locations.limit())
+            // await locations.forEach(loc => console.log(loc))
+            console.log(await locations)
+        }
     } catch(err){
         console.log(err)
     } finally {
@@ -31,6 +44,50 @@ app.post("/api1/:collection1", (req, res)=>{
     var connUrl = req.body.mongodbUrl;
     getData(connUrl, collection1, collection2).catch(console.error);
     res.send("Got it.");
+})
+
+function getLocation(addresses){
+    // var result = addresses.map(address => {
+    //     
+    //     request(url, { json: true }, (err, res, body) => {
+    //         if (err) { return console.log(err); }
+    //         coor = body.results[0].geometry.location
+    //         retValue = {
+    //             "add": body.results[0].formatted_address,
+    //             "location": [coor.lat, coor.lng]
+    //         }
+    //         console.log(retValue);
+    //         return retValue
+    //     });
+    // })
+}
+
+app.post("/api2", (req, res)=>{
+    var baseURL = "https://maps.googleapis.com/maps/api/geocode/json?address="
+    var apiKey = "AIzaSyA5bwbEsAOUMOI4RK2zXcIayG4vjuQSpcw"
+    var addresses = req.body.address
+    var result = []
+    addresses.forEach(address => {
+        var url = baseURL + address + "&key=" + apiKey;
+        axios.get(url)
+        .then(response => {
+            coor = response.data.results[0].geometry.location
+            retValue = {
+                "add": response.data.results[0].formatted_address,
+                "location": [coor.lat, coor.lng]
+            }
+            result.push(retValue);
+        })
+        .catch(error => {
+            console.log(error);
+        })
+        .finally(()=>{
+            if(result.length == addresses.length){
+                res.setHeader('Content-Type', 'application/json');
+                res.send(JSON.stringify(result));
+            }
+        })
+    })
 })
 
 app.listen(port, ()=>{
